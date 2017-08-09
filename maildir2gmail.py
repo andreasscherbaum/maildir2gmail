02fa130bdedb4e391cdcb57fae700f68878a8010
derGali <github@erlacher.net>
2017-08-09 16:41:07 +0200

    Allow dots in Folder Names

--- maildir2gmail.py
+++ maildir2gmail.py
@@ -58,7 +58,8 @@ class Gmail(object):
         timestamp = parsedate(message['date'])
         if not timestamp:
             log('Skipping "%s" - no date' % os.path.basename(filename))
-            return
+            timestamp = 0
+#           return
 
         subject = decode_header(message['subject'])
         log('Sending "%s" (%d bytes)' % (subject, len(content)))
@@ -69,7 +70,7 @@ class Gmail(object):
         else:
             self.imap.append(self.folder, '(\\Seen)', timestamp, content)
         self.mark_appended(filename)
-    
+
     def check_appended(self, filename):
         return os.path.basename(filename) in self.database
 
@@ -171,7 +172,7 @@ def main():
     options, args = parser.parse_args()
 
     # basic sanity check for folder name
-    if (re.match(r'^[a-zA-Z0-9 \[\]\-_\/]+\Z', options.folder) is None):
+    if (re.match(r'^[a-zA-Z0-9 \[\]\-_\/\.]+\Z', options.folder) is None):
         raise Exception('Invalid folder name')
 
     gmail = Gmail(options)
@@ -201,3 +202,4 @@ def parsedate(value):
 
 if __name__ == '__main__':
     main()
+
