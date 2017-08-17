214730147d7168e02654faf5a671eee6bf170f7f
derGali <github@erlacher.net>
2017-08-17 12:45:51 +0200

    Added useful readme-comments

--- maildir2gmail.sh
+++ maildir2gmail.sh
@@ -1,20 +1,26 @@
+#############
+# You may want to change or omit the String "Old Mail" 
+# You may want to change [Gmail]/Sent to your language, e.g. [Gmail]/Gesendet for german.
+# GMail username and password here
 username=USER@DOMAIN.COM
 passwd=PASSWORD
+#
+#############
 
 shopt -s extglob
 for dir in Maildir/.!(mu-size|Sent|Spam|Trash|Drafts)
 
 do
     echo "Directory:     $dir"
-        label=${dir//\./\/}
-        label=${label:9}
+    label=${dir//\./\/}
+    label=${label:9}
     echo "Converted to label: $label"
-    python /root/maildir2gmail.py -f "Alte Mails/$label" -u $username -p $passwd -n "$dir/new"
-    python /root/maildir2gmail.py -f "Alte Mails/$label" -u $username -p $passwd    "$dir/cur"
+    python /root/maildir2gmail.py -f "Old Mail/$label" -u $username -p $passwd -n "$dir/new"
+    python /root/maildir2gmail.py -f "Old Mail/$label" -u $username -p $passwd    "$dir/cur"
 
     echo
 done
 
-python /root/maildir2gmail.py -f "[Gmail]/Gesendet" -u $username -p $passwd   "Maildir/.Sent/cur"
+python /root/maildir2gmail.py -f "[Gmail]/Sent" -u $username -p $passwd   "Maildir/.Sent/cur"
 python /root/maildir2gmail.py -f "INBOX" -u $username -p $passwd   "Maildir/cur"
 python /root/maildir2gmail.py -f "INBOX" -u $username -p $passwd -n  "Maildir/new"
