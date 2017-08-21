13a12e1c54b61e07752604f30ad70ece30e87d08
derGali <github@erlacher.net>
2017-08-21 14:02:32 +0200

    Changes into local user's folder, Templates-Folder gets skipped

--- maildir2gmail.sh
+++ maildir2gmail.sh
@@ -2,13 +2,16 @@
 # You may want to change or omit the String "Old Mail" 
 # You may want to change [Gmail]/Sent to your language, e.g. [Gmail]/Gesendet for german.
 # GMail username and password here
+# localuser for changing into local user directory
 username=USER@DOMAIN.COM
 passwd=PASSWORD
+localuser=LOCALUSER
 #
 #############
 
 shopt -s extglob
-for dir in Maildir/.!(mu-size|Sent|Spam|Trash|Drafts)
+cd $localuser
+for dir in Maildir/.!(mu-size|Sent|Spam|Trash|Drafts|Templates)
 
 do
     echo "Directory:     $dir"
